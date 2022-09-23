801e1ea6bc34ff3eb92994bb3fd5982a39b11fe9
Daniel Cardozo <dani54196@gmail.com>
2022-09-23 18:05:23 -0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9ee38cf10a4cc37d8fb8.js"
+    "url": "webpack-runtime-1da88a7768892a94fcf4.js"
   },
   {
     "url": "framework-d24ffc6a52f0b7df5831.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "58087662728a3ea1df2a96ee3d2df695"
+    "revision": "904696178614cf0e64f8999e4408e58c"
   },
   {
     "url": "polyfill-0af18ce886c851496de4.js"
