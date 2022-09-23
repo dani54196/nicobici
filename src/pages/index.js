703e4220437dd9b4d707aaaab62ab08b2288809f
Daniel Cardozo <dani54196@gmail.com>
2022-09-23 15:42:53 -0300

    interface descripcion

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
-import Descripcion from '../components/Descripcion';
 
 import bic01 from '../assets/images/RockhopperEliteSpecialized.png';
 import bic02 from '../assets/images/RoubaixSportSpecialized.png';
@@ -35,26 +34,126 @@ const IndexPage = () => (
           </p>
         </header>
         <section className="tiles">
+          {/* /////////// */}
           <article className="style1">
-            <span className="image">
+            <span className="image ">
               <img src={bic01} alt="" />
             </span>
-            <Link to="/Generic2"></Link>
-            <Descripcion titulo="" imagen="" />
             <h2>Rockhopper Elite - Specialized</h2>
-            <div className="boton">
-              <div className="button primary small">$159,000</div>
+            <p>
+              <a class="cta" href="#google">
+                Saber mas
+              </a>
+            </p>
+            <h3>$159,000</h3>
+            <div id="google" class="modaloverlay">
+              <div class="modal">
+                <a href="#close" class="close">
+                  &times;
+                </a>
+                <div>
+                  <h1>Rockhopper Elite - Specialized</h1>
+                  <p>
+                    Cuadro de aluminio Premium A1 liviano pero duradero, el
+                    Rockhopper cuenta con tubos de aluminio conificados para
+                    mantener el peso bajo y la resistencia alta, al mismo tiempo
+                    que brinda mayor espacio libre para el soporte, enrutamiento
+                    interno de cables liso y compatibilidad con tija
+                    telescópica. Con el objetivo de asegurar de que Rockhopper
+                    ofrezca el mejor ajuste y el mejor rendimiento para cada
+                    persona, sin importar su medida, han emparejado cada tamaño
+                    de cuadro Rockhopper con el tamaño de rueda óptimo. El
+                    resultado es una Rockhopper que se adapta a cada ciclista.
+                    *La horquilla Judy SoloAir de RockShox ofrece sensibilidad y
+                    rendimiento con suspensión neumática a las masas. El
+                    amortiguador TurnKey reparte hábilmente el recorrido
+                    permitiéndote concentrarte en lo que viene.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </article>
+          {/* /////////// */}
+          <article className="style1">
+            <span className="image ">
+              <img src={bic02} alt="" />
+            </span>
+            <h2>Roubaix Sport - Specialized</h2>
+            <p>
+              <a class="cta" href="#google">
+                Saber mas
+              </a>
+            </p>
+            <h3>$250,000</h3>
+            <div id="google" class="modaloverlay">
+              <div class="modal">
+                <a href="#close" class="close">
+                  &times;
+                </a>
+                <div>
+                  <h1>Roubaix Sport - Specialized</h1>
+                  <p>
+                    Con seis victorias en la París-Roubaix, la Roubaix ha
+                    comprobado que Más Suave es Más Rápido. Hasta ahora, sin
+                    embargo, admitimos que lo suave ha venido con algunas
+                    concesiones. Pero ya no. La nueva Roubaix Sport ahora ofrece
+                    elasticidad sin concesiones al incluir el nuevo y radical
+                    Future Shock 1.5 que te da más control sobre rutas con
+                    baches, además de un nuevo poste de asiento Pavé, para crear
+                    la Roubaix más equilibrada que hayamos hecho, con
+                    aerodinámica que iguala a la Tarmac y un cuadro Rider-First
+                    Engineered con peso realmente reducido. ¿Sigue siendo
+                    cómoda? Por supuesto, pero no diríamos que es una bici de
+                    confort, el foco de cada decisión de ingeniería que tomamos
+                    fue el rendimiento. Esta es la nueva Roubaix. Esta Roubaix
+                    Sport ofrece una espectacular relación calidad-precio, pero
+                    al igual que el cuadro, lo hace sin concesiones. En sus
+                    especificaciones incluimos el grupo 105 de 11 velocidades de
+                    Shimano para obtener cambios precisos y poderosos frenos de
+                    disco hidráulicos SRAM, así como ruedas DT R470 Disc
+                    confiables y fuertes, el flamante poste de asiento S-Works
+                    Pavé, llantas Turbo Pro de 28mm y nuestro popular asiento
+                    Power Sport .
+                  </p>
+                </div>
+              </div>
             </div>
           </article>
 
           <article className="style1">
-            <span className="image">
-              <img src={bic02} alt="" />
+            <span className="image ">
+              <img src={bic03} alt="" />
             </span>
-            <Link to="/Generic"></Link>
-            <h2>Roubaix Sport - Specialized</h2>
-            <div className="boton">
-              <div className="button primary small">$250,000</div>
+            <h2>Diverge Sport Carbon - Specialized</h2>
+            <p>
+              <a class="cta" href="#google">
+                Saber mas
+              </a>
+            </p>
+            <h3>$150,000</h3>
+            <div id="google" class="modaloverlay">
+              <div class="modal">
+                <a href="#close" class="close">
+                  &times;
+                </a>
+                <div>
+                  <h1>Diverge Sport Carbon - Specialized</h1>
+                  <p>
+                    Con un cuadro de carbono FACT 8r, el cuadro Diverge Sport es
+                    una bicicleta de carreras increíblemente capaz y liviana.
+                    Anótalo para tu KOM de escalada de tierra local, o escala un
+                    pico gigante en el campo, el Diverge ligero y animado te
+                    llevará allí más rápido. Velocidad, control y confianza
+                    Future Shock 1.5 es la tecnología ideal para el ciclista de
+                    gravel. Con 20 mm de elasticidad axial, Future Shock 1.5 se
+                    ajusta progresivamente específicamente para la conducción en
+                    grava. Esto te mantiene confiado y en control, capaz de
+                    mantener baja la potencia y no pisar los frenos en caminos
+                    de tierra accidentados y en grava gruesa y suelta, todo
+                    mientras mantienes tus manos, brazos y cuello frescos.
+                  </p>
+                </div>
+              </div>
             </div>
           </article>
 
@@ -62,9 +161,8 @@ const IndexPage = () => (
             <span className="image">
               <img src={bic03} alt="" />
             </span>
-            <Link to="/Generic">
-            </Link>
-              <h2>Diverge Sport Carbon - Specialized</h2>
+            <Link to="/Generic"></Link>
+            <h2>Diverge Sport Carbon - Specialized</h2>
             <div className="boton">
               <div className="button primary small">$150,000</div>
             </div>
@@ -74,9 +172,8 @@ const IndexPage = () => (
             <span className="image">
               <img src={bic04} alt="" />
             </span>
-            <Link to="/Generic">
-            </Link>
-              <h2>Tarmac SL6 Sport - Specialized</h2>
+            <Link to="/Generic"></Link>
+            <h2>Tarmac SL6 Sport - Specialized</h2>
             <div className="boton">
               <div className="button primary small">$150,000</div>
             </div>
@@ -86,9 +183,8 @@ const IndexPage = () => (
             <span className="image">
               <img src={bic05} alt="" />
             </span>
-            <Link to="/Generic">
-            </Link>
-              <h2>Sirrus X - Specialized</h2>
+            <Link to="/Generic"></Link>
+            <h2>Sirrus X - Specialized</h2>
             <div className="boton">
               <div className="button primary small">$120,000</div>
             </div>
@@ -98,9 +194,8 @@ const IndexPage = () => (
             <span className="image">
               <img src={bic06} alt="" />
             </span>
-            <Link to="/Generic">
-            </Link>
-              <h2>SCULTURA TEAM - Merida</h2>
+            <Link to="/Generic"></Link>
+            <h2>SCULTURA TEAM - Merida</h2>
             <div className="boton">
               <div className="button primary small">$192,000</div>
             </div>
@@ -110,9 +205,8 @@ const IndexPage = () => (
             <span className="image">
               <img src={bic07} alt="" />
             </span>
-            <Link to="/Generic">
-            </Link>
-              <h2>SCULTURA ENDURANCE 9000 - Merida</h2>
+            <Link to="/Generic"></Link>
+            <h2>SCULTURA ENDURANCE 9000 - Merida</h2>
             <div className="boton">
               <div className="button primary small">$267,500</div>
             </div>
@@ -122,9 +216,8 @@ const IndexPage = () => (
             <span className="image">
               <img src={bic08} alt="" />
             </span>
-            <Link to="/Generic">
-            </Link>
-              <h2>SPEEDER 200 - Merida</h2>
+            <Link to="/Generic"></Link>
+            <h2>SPEEDER 200 - Merida</h2>
             <div className="boton">
               <div className="button primary small">$200,000</div>
             </div>
@@ -134,9 +227,8 @@ const IndexPage = () => (
             <span className="image">
               <img src={bic09} alt="" />
             </span>
-            <Link to="/Generic">
-            </Link>
-              <h2>MATTS J. 24+ - Merida</h2>
+            <Link to="/Generic"></Link>
+            <h2>MATTS J. 24+ - Merida</h2>
             <div className="boton">
               <div className="button primary small">$247,000</div>
             </div>
@@ -146,9 +238,8 @@ const IndexPage = () => (
             <span className="image">
               <img src={bic10} alt="" />
             </span>
-            <Link to="/Generic">
-            </Link>
-              <h2>REACTO 8000 - Merida</h2>
+            <Link to="/Generic"></Link>
+            <h2>REACTO 8000 - Merida</h2>
             <div className="boton">
               <div className="button primary small">$211,000</div>
             </div>
